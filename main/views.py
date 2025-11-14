e2a8e12cdfb82a90ed2e6e2f5ef7c57cf24e443a
avalance4114 <avalance4114@gmail.com>
2025-11-14 21:59:18 +0900

    #12 feat:listPage 구현

--- main/views.py
+++ main/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def main(request: HttpRequest):
 
     return render(request, "main/main.html")
+
+def listPage(request: HttpRequest):
+    
+    return render(request, 'main/listPage.html')
+
+def detailPage(request: HttpRequest):
+    
+    return render(request, 'main/detailPage.html')

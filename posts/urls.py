0a5ae4be080d51a68f74b7997c41577a512e0fae
ggom121@naver.com <ggom121@naver.com>
2025-11-19 16:03:34 +0900

    feat: 필터링 버튼 구현

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,7 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.posts_list, name="posts_list"),
+    path("type/<str:type_code>/", views.posts_type_list, name="posts_type_list"),
     path("detail/<int:post_id>/", views.posts_detail, name="posts_detail"),
+    path("ajax_list/", views.posts_ajax_list, name="posts_ajax_list"),
 ]

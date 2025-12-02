239c8e0930164666ee1dd60d09069f63cc0fdf84
ggom121@naver.com <ggom121@naver.com>
2025-12-03 05:00:42 +0900

    fix: 이메일 문제 해결

--- config/settings.py
+++ config/settings.py
@@ -176,5 +176,5 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() in ("true", "1", "yes")
 
-DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST", "itformaition702@gmail.com")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 SERVER_EMAIL = os.environ.get("SERVER_EMAIL", DEFAULT_FROM_EMAIL)
